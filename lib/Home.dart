1fe947e1088b7da018448c8232396b7963415060
Abd-UlRahman <a.khalid5322@gmail.com>
2022-11-24 11:10:58 +0200

    Creating HomeScreen

---
+++ lib/Home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routename='HomeScreen';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Islamic App'),),
+    );
+  }
+}
