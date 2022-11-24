ca62c18a4fc5d7429878b07cba1e4a2eff6fd3e9
Abd-UlRahman <a.khalid5322@gmail.com>
2022-11-24 11:54:43 +0200

    Add button Nav Bar

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routename='HomeScreen';
+  static const String routename = 'HomeScreen';
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('islamic'),),
+      appBar: AppBar(
+        title: Text('islamic'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              label: '',
+          backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              label: '',
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              label: '',
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              label: '',
+              backgroundColor: Colors.blue),
+        ],
+      ),
     );
   }
 }
